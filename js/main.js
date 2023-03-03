f1d50b1bdb2bc151a9dbab9bbef39f4615945930
Kang Hyomin <103807515+singrum@users.noreply.github.com>
2023-03-03 21:48:37 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 class App{
     constructor(){
-        
+        this.startBtn = document.querySelector("#start");
+        this._setupStartBtn();
     }
+    _setupStartBtn(){
+        this.startBtn.addEventListener("click", ()=>{
+            this.values = this._getValues();
+            document.querySelector("#start-modal").style.display = "none";
+            document.querySelector("#main-body").style.display = "block";
+        })
+    }
+    _getValues(){
+        return {
+            name : document.querySelector("#name").value,
+            birth : document.querySelector("#birth").value,
+            death : document.querySelector("#death").value,
+        }
+    }
+
 }
 
 window.onload = function () {
