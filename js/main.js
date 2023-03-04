c6c662332e1cde44afea1cc09042cea2f8210858
Kang Hyomin <103807515+singrum@users.noreply.github.com>
2023-03-04 02:14:30 +0000

    x

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ class App{
         this.startBtn = document.querySelector("#start");
         this._setupStartBtn();
     }
+    getFloatFixed(value, fixed){
+        return (Math.round(value * 1e7) / 1e5).toPrecision(fixed)
+    }
     _setupStartBtn(){
         this.startBtn.addEventListener("click", ()=>{
             this.values = this._getValues();
@@ -21,19 +24,56 @@ class App{
     _setValues(){
 
         const birth = moment(this.values.birth);
+        
         const now = moment();
         const death = moment(this.values.death);
         const diffBirthNow = moment.duration(now.diff(birth));
         const diffNowDeath = moment.duration(death.diff(now));
         const diffBirthDeath = moment.duration(death.diff(birth));
+        const age = diffBirthNow.years()
+        const ageClass = (Math.floor(age / 10)) * 10;
         
-        document.querySelector("#profile-name").innerHTML = `${this.ageToClass1(diffBirthNow.years())}&nbsp<b>${this.values.name}</b>`
-        document.querySelector("#date-date").innerHTML = `${this.values.birth.replace(/-/g, ".")}<br>${this.values.death.replace(/-/g, ".")}`;
+        document.querySelector("#profile-name").innerHTML = `${this.ageToClass1(age)}&nbsp<b>${this.values.name}</b>`
+        document.querySelector("#date-date").innerHTML = `${birth.format("l")}<br>${death.format("l")}`;
         document.querySelector("#profile-remaindate").innerHTML = `${diffNowDeath.years()}년 ${diffNowDeath.months()}개월 ${diffNowDeath.days()}일 남았습니다.`
         document.querySelector("#dday-num").innerHTML = `D-${Math.floor(diffNowDeath.asDays())}`
         document.querySelector("#icon-text").innerHTML = `${this.yearToClass(birth.year())}`
-        document.querySelector("#ageclass-name").innerHTML = `${diffBirthNow.years()}세`
+        document.querySelector("#ageclass-name").innerHTML = `${age}세`
+        document.querySelector("#range-death").innerHTML = `${death.format('ll')}`
+        const totalRange = document.querySelector("#range-total");
+        [totalRange.value, totalRange.max] = [diffBirthNow.asMilliseconds(),diffBirthDeath.asMilliseconds()]
+        const totalRangeInfo = document.querySelector("#total-range-info")
+        totalRangeInfo.innerHTML = `${this.getFloatFixed(totalRange.value / totalRange.max, 7)}%`
+        totalRangeInfo.style.transform = `translate(calc((100% - 67px) * ${totalRange.value / totalRange.max}), -2px)`
+        
 
+        document.querySelector("#age-date").innerHTML = `${birth.clone().add(age + 1, "y").format("l")}`
+        document.querySelector("#next-age").innerHTML = `${age + 1}`
+        const ageRange = document.querySelector("#subrange-age-bar");
+        ageRange.value = moment.duration(now.diff(birth.clone().add(age, "y"))).asMilliseconds();
+        ageRange.max = moment.duration(birth.clone().add(age + 1, "y").diff(birth.clone().add(age , "y"))).asMilliseconds()
+        const ageRangeInfo = document.querySelector("#age-range-info")
+        ageRangeInfo.innerHTML = `${this.getFloatFixed(ageRange.value / ageRange.max, 7)}%`
+        // ageRangeInfo.style.transform = `translate(${(window.innerWidth - 80 - 67) * ageRange.value / ageRange.max}px, -10px)`
+
+
+        document.querySelector("#class-date").innerHTML = `${birth.clone().add(ageClass + 10, "y").format("l")}`
+        document.querySelector("#next-class").innerHTML = `${ageClass + 10}대`
+        const classRange = document.querySelector("#subrange-class-bar");
+        classRange.value = moment.duration(now.diff(birth.clone().add(ageClass, "y"))).asMilliseconds();
+        classRange.max = moment.duration(birth.clone().add(ageClass + 10, "y").diff(birth.clone().add(ageClass , "y"))).asMilliseconds();
+        const classInfo = document.querySelector("#class-range-info")
+        classInfo.innerHTML = `${this.getFloatFixed(classRange.value / classRange.max, 7)}%`
+        // classInfo.style.transform = `translate(calc((100% - 67px) * ${classRange.value / classRange.max}), -10px)`
+
+        const footerValues = document.querySelectorAll(".main-right")
+        footerValues[0].innerHTML = diffBirthDeath.asDays()
+        footerValues[1].innerHTML = Math.floor(diffBirthNow.asDays());
+        footerValues[2].innerHTML = Math.floor(diffNowDeath.asDays());
+        footerValues[3].innerHTML = Math.floor(diffNowDeath.asWeeks());
+        footerValues[3].innerHTML = Math.floor(diffNowDeath.asMonths());
+        console.log(diffNowDeath)
+        
     }
     ageToClass1(age){
         switch(parseInt(age / 10)){
